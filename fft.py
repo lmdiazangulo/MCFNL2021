545eefbf7084a59fff4293a1fba22d037c98ad7d
Manuel <manuelmayoleon09@gmail.com>
2021-03-16 21:55:09 +0100

    mi primer commit

--- fft.py
+++ fft.py
@@ -5,12 +5,25 @@ import math
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 from scipy import optimize
+
+
 s0=10e-6
 t0=10*s0
 def gaussian(x,mu,sig):
     return np.exp(-np.power(x-mu,2.)/(2*np.power(sig,2.)))
-x_values =np.linspace(0,500e-6,num=(1000+1))
-plt.plot(x_values,gaussian(x_values,t0,s0))
-
-
+t =np.linspace(0,500e-6,num=(1000+1))
+plt.subplot(1, 2, 1)
+plt.plot(t,gaussian(t,t0,s0))
+plt.grid(color='k', linestyle='--', linewidth=0.5)
+plt.ylabel('g(t) ')
+plt.xlabel('t')
+plt.title('Funcion Gaussiana')
+plt.xlim(0,200e-6)
+freq = np.fft.fftfreq(t.shape[-1])
+gft=np.fft.fft(gaussian(t,t0,s0))
+plt.subplot(1, 2, 2)
+plt.plot(freq,abs(gft),color='r')
+plt.grid(color='k', linestyle='--', linewidth=0.5)
+plt.ylabel('G(w) ')
+plt.xlabel('w')
 plt.show()
