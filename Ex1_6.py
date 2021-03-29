ed95ed219fe4f0c3f3ab6926ed7d6a481a420093
Gregarval <oirogerg291098@gmail.com>
2021-03-29 17:20:17 +0200

    Ejercicio 1.6

--- Ex1_6.py
+++ Ex1_6.py
@@ -1,51 +1,93 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math as m
-import cmath as cm
+from scipy.constants import epsilon_0, mu_0
+
 # Ejercicio 1.6 Coeficientes de transmision y reflexion
 
-def eps(epsr,sigma,f):
-    ''' Funcion que devuelve la permitividad efectiva complega
-    con parametros la perm. relativa, conductividad y frec. '''
-    eps0=8.85e-12 # C^2/(N*m^2)
-    perm=epsr*eps0-sigma/(2*m.pi*f)*1j
-    return perm
+eta_0=np.sqrt(mu_0/epsilon_0)
 
-# Constantes
-
-eps0=8.85e-12 # C^2/(N*m^2)
-mu0=4*m.pi*1e-7 # T*m*A^-1
-
-# Propiedades del medio 1
-epsr=1
-d=10e-3
-sigma=10e3
-
-f=np.linspace(1e3,1e9,1000000)
-T=np.zeros(len(f),dtype=np.complex_)
-R=T
-
-for i in [0,1,len(f)-1]:
-
-    perm=eps(epsr,sigma,f[i])
-    gamma=1j*(2*m.pi)*f[i]*cm.sqrt(mu0*perm)
-    eta=cm.sqrt(mu0/perm)
-
-    phi00=cm.cosh(gamma*d)
-    phi11=phi00
-    phi01=eta*cm.sinh(gamma*d)
-    phi10=1/eta*cm.sinh(gamma*d)
+class Panel:
+    def __init__(self, thickness, epsilon_r, sigma, mu_r):
+        self.thickness = thickness
+        self.epsilon_r = epsilon_r
+        self.mu_r = mu_r
+        self.sigma = sigma
     
-    T[i]=2*eta/(phi00*eta+phi01+phi10*eta*eta+phi11*eta)
-    R[i]=(phi00*eta+phi01-phi10*eta*eta-phi11*eta)/(phi00*eta+phi01+phi10*eta*eta+phi11*eta)
+    def epsilon_c(self, omega):
+        return self.epsilon_r*epsilon_0 - complex(0,1)*self.sigma/omega
 
-fig, (ax1, ax2) = plt.subplots(2)
-ax1.set_title('Coeficiente transmision')
-ax1.plot(f,T)
-ax2.set_title('Coeficiente reflexion')
-ax2.plot(f,R)
+    def mu_c(self, omega):
+        return self.mu_r * mu_0
+
+    def gamma(self, omega):
+        return complex(0,1) * omega * np.sqrt(self.mu_c(omega) * self.epsilon_c(omega))
+    
+    def eta(self, omega):
+        return np.sqrt(self.mu_c(omega)/self.epsilon_c(omega))
+
+    def phi(self, omega):
+        gd=self.gamma(omega) * self.thickness
+        eta=self.eta(omega)
+
+        return np.array([ [np.cosh(gd), np.sinh(gd) * eta], \
+            [np.sinh(gd) / eta, np.cosh(gd)]  ])
+
+    def _den(self,omega):
+        phi=self.phi(omega)
+        return phi[0,0]*eta_0 + phi[0,1] + phi[1,0]*eta_0*eta_0 + phi[1,1]*eta_0
+
+    def T(self,omega):
+        return 2*eta_0 / self._den(omega)
+
+    def R(self,omega):
+        phi=self.phi(omega)
+        return (phi[0,0]*eta_0 + phi[0,1] - phi[1,0]*eta_0*eta_0 - phi[1,1]*eta_0) / self._den(omega)
+    
+
+
+Apartado1=Panel(100e-6,5,0,1)
+Apartado2=Panel(10e-3,5,0,1)
+Apartado3=Panel(100e-6,1,10e3,1)
+Apartado4=Panel(10e-3,1,10e3,1)
+
+
+omega=np.linspace(1e3,1e9,1000)
+
+plt.figure('Apartado 1')
+plt.subplot(2,1,1)
+plt.title('Coeficiente de reflexion')
+plt.plot(2 * np.pi * omega, np.abs(Apartado1.R(omega)))
+plt.subplot(2,1,2)
+plt.title('Coeficiente de transmision')
+plt.plot(2 * np.pi * omega, np.abs(Apartado1.T(omega)))
+
+
+plt.figure('Apartado 2')
+plt.subplot(2,1,1)
+plt.title('Coeficiente de reflexion')
+plt.plot(2 * np.pi * omega, np.abs(Apartado2.R(omega)))
+plt.subplot(2,1,2)
+plt.title('Coeficiente de transmision')
+plt.plot(2 * np.pi * omega, np.abs(Apartado2.T(omega)))
+
+
+plt.figure('Apartado 3')
+plt.subplot(2,1,1)
+plt.title('Coeficiente de reflexion')
+plt.plot(2 * np.pi * omega, np.abs(Apartado3.R(omega)))
+plt.subplot(2,1,2)
+plt.title('Coeficiente de transmision')
+plt.plot(2 * np.pi * omega, np.abs(Apartado3.T(omega)))
+
+
+plt.figure('Apartado 4')
+plt.subplot(2,1,1)
+plt.title('Coeficiente de reflexion')
+plt.plot(2 * np.pi * omega, np.abs(Apartado4.R(omega)))
+plt.subplot(2,1,2)
+plt.title('Coeficiente de transmision')
+plt.plot(2 * np.pi * omega, np.abs(Apartado4.T(omega)))
 plt.show()
-
 
 
     
